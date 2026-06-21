95b2c4a6586e535eb7ddc1ff9f048728cd5e9fff
sarithakommisetti392000@gmail.com <sarithakommisetti392000@gmail.com>
2026-06-21 23:17:33 +0530

    Added new file app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from src.query import query_rag_pipeline
+
+st.set_page_config(page_title="Document Q&A Bot")
+
+st.title("📄 RAG Document Q&A Bot")
+
+question = st.text_input("Ask a question from your documents")
+
+if st.button("Get Answer"):
+    if question.strip():
+        result = query_rag_pipeline(question)
+        st.subheader("Answer")
+        st.write(result["answer"])
